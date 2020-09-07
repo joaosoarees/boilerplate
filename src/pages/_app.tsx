6a3f30a6c1a1309d2bf719d25a709b6ae69b0f1d
João Vitor <jsoareez@gmail.com>
2020-09-07 09:53:19 -0300

    boilerplate

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from '../styles/GlobalStyles';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default App;
